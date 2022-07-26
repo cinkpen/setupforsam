db646bcca9ee5f32cbcf0e08f5d0c66299e4544f
Chris Inkpen <chris.inkpen@gmail.com>
2022-07-26 07:47:02 +0100

    First OPen SSL running

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+console.log("Hello Sam");
 
-console.log("Hello world");
-
+const openssl = require("openssl-nodejs");
+openssl("c:\\Program Files\\OpenSSL-Win64\\bin\\openssl req -config csr.cnf -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout key.key -out certificate.crt");
